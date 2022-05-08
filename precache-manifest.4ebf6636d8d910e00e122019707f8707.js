42d047a7ed9ed3ed12ebb1374a490ebdf3d9c19c
miyamotokz <96810841+miyamotokz@users.noreply.github.com>
2022-05-09 01:57:42 +0900

    Updates

---
+++ precache-manifest.4ebf6636d8d910e00e122019707f8707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4abde740797f3898bdbf",
+    "url": "/codingtest/static/css/main.9afd9c17.chunk.css"
+  },
+  {
+    "revision": "4abde740797f3898bdbf",
+    "url": "/codingtest/static/js/main.1e19f524.chunk.js"
+  },
+  {
+    "revision": "90be4d5e6832b6608569",
+    "url": "/codingtest/static/js/runtime~main.a105a2f9.js"
+  },
+  {
+    "revision": "3e41a0da32d08babcaf3",
+    "url": "/codingtest/static/js/2.7b992a7d.chunk.js"
+  },
+  {
+    "revision": "b2871250ed127b512992",
+    "url": "/codingtest/static/js/3.6f35523b.chunk.js"
+  },
+  {
+    "revision": "ef1262932299a4d499b1e32536a02685",
+    "url": "/codingtest/index.html"
+  }
+];
