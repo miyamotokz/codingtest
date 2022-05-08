4637178ef24b23dc5170138cc069d1596cb4c851
miyamotokz <96810841+miyamotokz@users.noreply.github.com>
2022-05-09 00:07:24 +0900

    pageHeader修正

--- src/components/pageHeader.tsx
+++ src/components/pageHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../App.css'
 
 const pageHeader: React.FC = () => {
-  return <>title</>
+  return <header className="pageHeader">title</header>
 }
 
 export default pageHeader
